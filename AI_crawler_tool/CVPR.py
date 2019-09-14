0ccfa0bd066042bdc23b076907901698452f5380
e <hongqiang601217@qq.com>
2019-09-14 15:21:40 +0800

    CVPR Conference Introduce

---
+++ AI_crawler_tool/CVPR.py
@@ -0,0 +1,5 @@
+# The Conference on Computer Vision and Pattern Recognition (CVPR)
+# is an annual conference on computer vision and pattern recognition,
+# which is regarded as one of the most important conferences in its field.
+# Conference website: http://cvpr2020.thecvf.com/
+# Paper link : http://openaccess.thecvf.com/menu.py
